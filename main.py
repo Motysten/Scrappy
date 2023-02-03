a802e32fcf816507d5e21cc47aa9d09d9101e40e
Motysten <mateo.demuynck@gmail.com>
2023-02-03 11:17:56 +0100

    Update main.py

--- main.py
+++ main.py
@@ -10,8 +10,11 @@ if response.ok:
     soup = bs4.BeautifulSoup(response.text, 'html.parser')
     div = soup.find('div', {"id" : "request-container"})
     list = div.findAll('div', {"class" : "card ombre h-100"})
+    urls = []
     for name in list:
         print('Nom : ' + name.find('h5').contents[0])
+        print('URL : ' + baseUrl + name.find('a').attrs['href'])
+        urls.append(baseUrl + name.find('a').attrs['href'])
 
         try:
             print('Places : ' + str(int(name.find('h2').contents[0].replace(' places libres', ''))) + '/' + name.find('p').find('small').contents[0].replace('capacité : ', '').replace(' places', ''))
@@ -21,3 +24,4 @@ if response.ok:
             else:
                 print('Impossible d\'obtenir le suivi de ce parking !')
         print('\n')
+    print(urls)
