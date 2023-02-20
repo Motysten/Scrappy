342c898bb6cca1870202c58fb145fcb65d798a85
Motysten <mateo.demuynck@gmail.com>
2023-02-20 14:59:57 +0100

    Update main.py

--- main.py
+++ main.py
@@ -17,27 +17,22 @@ if response.ok:
         infos = {}
 
         print('Nom : ' + name.find('h5').contents[0])
-        # infos.append(name.find('h5').contents[0])
         infos["Nom"] = name.find('h5').contents[0]
 
         print('URL : ' + baseUrl + name.find('a').attrs['href'])
-        # infos.append(baseUrl + name.find('a').attrs['href'])
         infos["Lien"] = baseUrl + name.find('a').attrs['href']
         
         urls.append(baseUrl + name.find('a').attrs['href'])
 
         try:
             print('Places : ' + str(int(name.find('h2').contents[0].replace(' places libres', ''))) + '/' + name.find('p').find('small').contents[0].replace('capacité : ', '').replace(' places', ''))
-            # infos.append(str(int(name.find('h2').contents[0].replace(' places libres', ''))) + '/' + name.find('p').find('small').contents[0].replace('capacité : ', '').replace(' places', ''))
             infos["Places"] = str(int(name.find('h2').contents[0].replace(' places libres', ''))) + '/' + name.find('p').find('small').contents[0].replace('capacité : ', '').replace(' places', '')
         except:
             if name.find('h2').contents[0].replace(' places libres', '') == 'INDISPONIBLE':
                 print('Places : 0/' + name.find('p').find('small').contents[0].replace('capacité : ', '').replace(' places', ''))
-                # infos.append('0/' + name.find('p').find('small').contents[0].replace('capacité : ', '').replace(' places', ''))
                 infos['Places'] = '0/' + name.find('p').find('small').contents[0].replace('capacité : ', '').replace(' places', '')
             else:
                 print('Impossible d\'obtenir le suivi de ce parking !')
-                # infos.append("Error !")
                 infos['Places'] = "Error !"
         print('\n')
 
